d86a1f729e1a05808b39a1acb7216239473cc2d2
jafar <jafaralituraev@gmail.com>
2022-11-09 23:19:38 +0500

    temp

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+import Design from '../components/Layout/Pages/Design'
+
+function Index(props) {
+    return (
+    <Layout>
+
+    </Layout>
+    )
+}
+
+export default Index
